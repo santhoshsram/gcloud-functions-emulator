53a610b31cf59648997acc6668d6c10b026b8a6f
Santhosh Sundararaman <santhoshsram@gmail.com>
2018-05-20 19:52:52 +0530

    Add API to return dummy status of emulator

--- api/apiserver.py
+++ api/apiserver.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 apiserver = Flask(__name__)
 
-@apiserver.route("/")
-def getHello():
-    return "Hello, world!"
+emulator =  {
+                'status'    : u'RUNNING',
+                'uptime'    : u'1min',
+                'version'   : u'1.0beta'
+            }
+
+@apiserver.route("/functions-emulator/v1/emulator", methods=['GET'])
+def get_emulator():
+    return jsonify({'emulator': emulator})
 
 if __name__ == "__main__":
     apiserver.run()
